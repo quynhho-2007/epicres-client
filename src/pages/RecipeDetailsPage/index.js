ad1a08ea37e45ccf851e3224dc9d40d66ecbfd5b
quynhho-2007 <hovuthaoquynh@gmail.com>
2021-04-12 15:04:27 +0200

    Display info on recipe details page

--- src/pages/RecipeDetailsPage/index.js
+++ src/pages/RecipeDetailsPage/index.js
@@ -43,18 +43,40 @@ export default function RecipeDetailsPage() {
           console.log(i);
           return <p>{i.title}</p>;
         })} */}
+      </Row>
+      <Row>
         <Col>
-          <Card>
-            {specificRecipe.ingredients?.map((i) => {
-              return <Card> {i.title}</Card>;
-            })}
-
-            {/* <Card.Title>{i.title}</Card.Title>
-            <Card.Text>
-              <p>Total Price: {specificRecipe.totalPrice}</p>
-              <p>Calories: {specificRecipe.totalCalories}</p>
-              <p>Purchase times: {specificRecipe.bought} </p>
-            </Card.Text> */}
+          <Card style={{ textAlign: "center", backgroundColor: "#DEE1DD" }}>
+            <Card.Img
+              src="https://www.clariant.com/-/media/Images/Business-Units/ICS/Food-Ingredients/New/Clariant-image-food-ingredients.jpg
+            "
+            />
+            <Card.ImgOverlay>
+              <Card.Title>Tags</Card.Title>
+              {specificRecipe.tags?.map((i) => {
+                return <Card.Text>{i.title}</Card.Text>;
+              })}
+            </Card.ImgOverlay>
+          </Card>
+          <Card style={{ textAlign: "center" }}>
+            <Card.Img src="https://previews.123rf.com/images/sauvignon/sauvignon1510/sauvignon151000068/47381940-baking-cake-ingredients-background-.jpg" />
+            <Card.ImgOverlay>
+              <Card.Title> Ingredients </Card.Title>
+              {specificRecipe.ingredients?.map((i) => {
+                return (
+                  <Card.Text style={{ borderTop: "30px" }}>{i.title}</Card.Text>
+                );
+              })}
+            </Card.ImgOverlay>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ textAlign: "center", color: "black" }}>
+            <Card.Img src="https://images.unsplash.com/photo-1495195134817-aeb325a55b65?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0dGluZyUyMGJvYXJkfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80" />
+            <Card.ImgOverlay>
+              <Card.Title>Instruction</Card.Title>
+              <Card.Text>{specificRecipe.instruction}</Card.Text>
+            </Card.ImgOverlay>
           </Card>
         </Col>
       </Row>
