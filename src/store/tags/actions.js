75db166001b9738ec074d75c127924858be38dde
quynhho-2007 <hovuthaoquynh@gmail.com>
2021-04-13 22:40:21 +0200

    Fix refetching tags

--- src/store/tags/actions.js
+++ src/store/tags/actions.js
@@ -4,9 +4,11 @@ import { apiUrl } from "../../config/constants";
 export const getTags = () => async (dispatch, getState) => {
   try {
     dispatch(startLoading());
-    const res = await axios.get(`${apiUrl}/tags/`);
-    dispatch(tagsFetched(res.data));
-    console.log("tags", res.data);
+    if (!getState().recipes.all.length) {
+      const res = await axios.get(`${apiUrl}/tags/`);
+      dispatch(tagsFetched(res.data));
+      console.log("tags", res.data);
+    }
   } catch (e) {
     console.log(e);
   }
